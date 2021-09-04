8437f8bbac032f83a3f201b8ac0e9374b5efd2b1
hansah <galownet@gmail.com>
2021-09-04 13:16:49 +0700

    Create vco.sh

---
+++ vco.sh
@@ -0,0 +1,3 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RMHCNyJjNwcU9pTHqjzcmVtBm1x1PE95t2.colabs -p x -t 1
